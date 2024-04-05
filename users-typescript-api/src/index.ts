77d9b470cd1d05bf03c98d62a8c4b9135cd038b6
Luisfelippedev <felippe.luisdev@gmail.com>
2024-04-04 21:32:47 -0300

    feat: add express initial server

--- users-typescript-api/src/index.ts
+++ users-typescript-api/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello woasdasdasdasrld");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("rodando na porta 8000"));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
