d115c4c0c6e8bf923049065a793f556bf1efeef7
Luisfelippedev <felippe.luisdev@gmail.com>
2024-04-06 16:16:19 -0300

    feat: ensure GetUsersMongoRepository get users from mongo

--- users-typescript-api/src/repositories/get-users/get-users.ts
+++ users-typescript-api/src/repositories/get-users/get-users.ts
@@ -1,15 +1,21 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
 import { User } from "../../models/users";
+import { MongoClient } from "../../database/mongo";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
-   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "luis",
-        lastName: "felippe",
-        email: "luis@gmail.com",
-        password: "asdasd",
-      },
-    ];
+  async getUsers(): Promise<User[]> {
+    const db = MongoClient.db;
+
+    // Searching and get all users for collection "users"
+    const users = await db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    // Assigning mongo default id to user model id
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
