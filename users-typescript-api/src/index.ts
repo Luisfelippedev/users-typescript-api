f772a5cd83fd25f7e8afa39a633a96351b570ca2
Luisfelippedev <felippe.luisdev@gmail.com>
2024-04-06 01:53:59 -0300

    feat: add route /users for get users

--- users-typescript-api/src/index.ts
+++ users-typescript-api/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
+import { GetUsersController } from "./controllers/get-users/get-users";
+import { MongoGetUsersRepository } from "./repositories/get-users/get-users";
+import { IGetUsersRepository } from "./controllers/get-users/protocols";
 
 config();
 
@@ -10,4 +13,12 @@ app.listen(port, () => console.log("rodando na porta 8000"));
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.get("/users", async (req, res) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+  const { body, statusCode } = await getUsersController.handle();
+
+  res.send(body).status(statusCode);
 });
